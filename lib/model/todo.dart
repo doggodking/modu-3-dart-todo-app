a470c08ee4ab87eeeaa33dd9d66b00683ae45a34
Junsung Choi <choiexe1@gmail.com>
2025-04-02 13:42:27 +0900

    feat: model class todo

---
+++ lib/model/todo.dart
@@ -0,0 +1,70 @@
+class Todo implements Comparable<Todo> {
+  final int userId;
+  final int id;
+  final String title;
+  final bool completed;
+  final DateTime createdAt;
+
+  const Todo({
+    required this.userId,
+    required this.id,
+    required this.title,
+    required this.completed,
+    required this.createdAt,
+  });
+
+  factory Todo.fromJson(Map<String, dynamic> json) {
+    return Todo(
+      id: json['id'],
+      completed: json['completed'],
+      createdAt: DateTime.parse(json['createdAt']),
+      title: json['title'],
+      userId: json['userId'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'completed': completed,
+      'title': title,
+      'createdAt': createdAt,
+      'userId': userId,
+    };
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        other is Todo && other.hashCode == this.hashCode;
+  }
+
+  @override
+  String toString() {
+    return 'Todo(id: $id, userId: $userId, title: $title, completed: $completed, createdAt: $createdAt)';
+  }
+
+  @override
+  int get hashCode => id.hashCode;
+
+  Todo copyWith({
+    int? userId,
+    int? id,
+    String? title,
+    bool? completed,
+    DateTime? createdAt,
+  }) {
+    return Todo(
+      userId: userId ?? this.userId,
+      id: id ?? this.id,
+      title: title ?? this.title,
+      completed: completed ?? this.completed,
+      createdAt: createdAt ?? this.createdAt,
+    );
+  }
+
+  @override
+  int compareTo(Todo other) {
+    return this.createdAt.compareTo(other.createdAt);
+  }
+}
