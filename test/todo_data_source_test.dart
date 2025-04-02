8335a9206d1a3355153945428ac8399f2bbe6a3a
Yunseo <og737596@gmail.com>
2025-04-02 17:03:10 +0900

    feat : todoDataSourcTestcode

--- test/todo_data_source_test.dart
+++ test/todo_data_source_test.dart
@@ -6,15 +6,64 @@ import 'package:todo_app/data_source/todo_data_source.dart';
 import 'package:todo_app/data_source/todo_data_source_impl.dart';
 
 void main() async {
-  File backUpfile = File('data/backup.dat');
-  List backUpList = jsonDecode(await backUpfile.readAsString());
-  List<Map<String, dynamic>> backUpMapList =
-      backUpList.map((e) => e as Map<String, dynamic>).toList();
+  const String filePath = 'data/todos.json';
 
-  test("1: 파일이 존재하지 않는 경우 자동 생성 및 초기화", () async {
-    TodoDataSource todoDataSource = TodoDataSourceImpl(path: "data/todos.json");
-    List<Map<String, dynamic>> toDoList = await todoDataSource.readTodos();
+  Future<List<Map<String, dynamic>>> readFile({required String path}) async {
+    List readFileList = jsonDecode(await File(path).readAsString());
+    List<Map<String, dynamic>> readFileData =
+        readFileList.map((items) => items as Map<String, dynamic>).toList();
 
-    expect(toDoList, equals(backUpMapList));
+    return readFileData;
+  }
+
+  test("1. 파일 미존재 시 – 자동 파일 생성 및 백업 사용", () async {
+    TodoDataSource todoDataSource = TodoDataSourceImpl(path: filePath);
+    List<Map<String, dynamic>> getDataSource = await todoDataSource.readTodos();
+
+    if (!await File(filePath).exists()) throw Exception();
+    if (await File(filePath).exists()) {
+      expect(getDataSource, equals(await readFile(path: 'data/backup.dat')));
+    }
+  });
+
+  test("2. readTodos() – 정상 파일에서 데이터 읽기", () async {
+    TodoDataSource todoDataSource = TodoDataSourceImpl(path: filePath);
+    List<Map<String, dynamic>> getDataSource = await todoDataSource.readTodos();
+    expect(getDataSource, equals(await readFile(path: 'data/todos.json')));
+  });
+
+  test("3. writeTodos() – 데이터 파일에 쓰기", () async {
+    TodoDataSource todoDataSource = TodoDataSourceImpl(path: filePath);
+    List<Map<String, dynamic>> readData = await todoDataSource.readTodos();
+    List<Map<String, dynamic>> mockData = [
+      {
+        "userId": 1,
+        "id": 1,
+        "title": "생존코딩 유튜브 구독하기",
+        "completed": false,
+        "createdAt": "2025-03-29T10:15:00Z",
+      },
+      {
+        "userId": 1,
+        "id": 2,
+        "title": "PR 제출하기",
+        "completed": false,
+        "createdAt": "2025-03-30T08:30:00Z",
+      },
+      {
+        "userId": 1,
+        "id": 3,
+        "title": "다른 사람 코드 리뷰하기",
+        "completed": false,
+        "createdAt": "2025-03-31T14:00:00Z",
+      },
+    ];
+
+    await todoDataSource.writeTodos(mockData);
+
+    expect(
+      await todoDataSource.readTodos(),
+      equals([...readData, ...mockData]),
+    );
   });
 }
